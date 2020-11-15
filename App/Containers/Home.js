bf2906c9a292112977ad6a83ecd4d3c4889f1027
jqn <jqngl@icloud.com>
2020-11-15 09:10:55 +0100

    Added screen containers home, quiz and results

---
+++ App/Containers/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View } from 'react-native';
+
+class Home extends Component {
+  state = {  }
+  render() {
+    return (
+      
+    );
+  }
+}
+
+export default Home;
